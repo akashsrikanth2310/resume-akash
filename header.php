673fec62112a997bb4fd244d20633a49b064e96b
Akash Srikanth <akashsrikanth14@gmail.com>
2020-10-04 23:54:31 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+//* Do NOT include the opening php tag
+
+//* Load Font Awesome
+add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
+function enqueue_font_awesome() {
+
+    wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
+
+}
